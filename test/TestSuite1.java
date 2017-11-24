22937e0c2a245833435f4bb1ab3901b5b994510f
thelaua <jiananliu@umass.edu>
2017-11-24 18:12:55 -0500

    Add Test Cases for the other three files on CompileSrcTarget()

--- test/TestSuite1.java
+++ test/TestSuite1.java
@@ -5,15 +5,21 @@ import java.io.File;
 import org.junit.Test;
 
 public class TestSuite1 {
-	File file = new File("test/TestBuildFile1.xml");
-	AntBuildAnalyzer aba = new AntBuildAnalyzer(file);
+	
+	AntBuildAnalyzer aba0 = new AntBuildAnalyzer(new File("test/TestBuildFile0.xml"));
+	AntBuildAnalyzer aba1 = new AntBuildAnalyzer(new File("test/TestBuildFile1.xml"));
+	AntBuildAnalyzer aba2 = new AntBuildAnalyzer(new File("test/TestBuildFile2.xml"));
+	AntBuildAnalyzer aba3 = new AntBuildAnalyzer(new File("test/TestBuildFile3.xml"));
 	
 	
 	
 	@Test
 	public void srcCompileTarget() {
 		
-		assertEquals("compile", aba.getCompileSrcTarget());
+		assertEquals("compile", aba0.getCompileSrcTarget());
+		assertEquals("compile", aba1.getCompileSrcTarget());
+		assertEquals("build", aba2.getCompileSrcTarget());
+		assertEquals("compile", aba3.getCompileSrcTarget());
 	}
 
 }
