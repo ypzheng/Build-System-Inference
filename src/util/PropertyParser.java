9b9f08db8f15fef2d7cdc0008290b8fdcf614d94
Jucong <hejucong1995@gmail.com>
2017-12-04 15:21:12 -0500

    remove redundant code

--- src/util/PropertyParser.java
+++ src/util/PropertyParser.java
@@ -9,27 +9,18 @@ import java.util.Map;
 
 public class PropertyParser {
     private Project myProject;
-    private Map<String,String> property_map;
+    private Hashtable<String, Object> property_map;
     public PropertyParser(Project p) {
         myProject = p;
-        property_map = convert2String(myProject.getProperties());
+        property_map = myProject.getProperties();
     }
 
     public String get_property(String name) {
         if (property_map.containsKey(name)) {
-            return property_map.get(name);
+            return myProject.getProperty(name);
         } else {
             return null;
         }
     }
 
-    private Map<String,String> convert2String(Hashtable<String,Object> property_table) {
-        Enumeration<String> keys = property_table.keys();
-        Map<String,String> tempMap = new HashMap<>();
-        while (keys.hasMoreElements()) {
-            String key = keys.nextElement();
-            tempMap.put(key,property_table.get(key).toString());
-        }
-        return tempMap;
-    }
 }
