54a2c1ffe28d859e2a7acd657b17826eeafc32ce
ypzheng <ypzheng@umass.edu>
2017-12-07 23:16:36 -0500

    Minor clean up.

--- src/BuildFileAnalyzerAdapter.java
+++ src/BuildFileAnalyzerAdapter.java
@@ -14,55 +14,46 @@ public class BuildFileAnalyzerAdapter implements BuildAnalyzer{
 	
 	@Override
 	public String getCompileSrcTarget() {
-		// TODO Auto-generated method stub
 		return analyzer.getCompileSrcTarget();
 	}
 
 	@Override
 	public String getCompileTestTarget() {
-		// TODO Auto-generated method stub
 		return analyzer.getCompileTestTarget();
 	}
 
 	@Override
 	public String getSrcDir() {
-		// TODO Auto-generated method stub
 		return analyzer.getSrcDir();
 	}
 
 	@Override
 	public String getTestDir() {
-		// TODO Auto-generated method stub
 		return analyzer.getTestDir();
 	}
 
 	@Override
 	public String getCompDir() {
-		// TODO Auto-generated method stub
 		return analyzer.getCompDir();
 	}
 
 	@Override
 	public String getCompTestDir() {
-		// TODO Auto-generated method stub
 		return analyzer.getCompTestDir();
 	}
 
 	@Override
 	public String getSrcDep() {
-		// TODO Auto-generated method stub
 		return analyzer.getSrcDep();
 	}
 
 	@Override
 	public String getTestDep() {
-		// TODO Auto-generated method stub
 		return analyzer.getTestDep();
 	}
 
 	@Override
 	public String getTestList() {
-		// TODO Auto-generated method stub
 		return analyzer.getTestList();
 	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -7,18 +7,6 @@ import java.nio.file.Paths;
 public class Driver {
 
 	public static void main(String[] args) throws IOException {
-//		Ex_Get_Depend parser = new Ex_Get_Depend();
-//        Map<String, ArrayList<String>> dependencies = parser.getDependencies();
-//        for(String key : dependencies.keySet()) {
-//            ArrayList<String> values = dependencies.get(key);
-//            System.out.print(key +" is dependent on ");
-//            for (String d: values) {
-//                System.out.print(d+", ");
-//            }
-//            System.out.println();
-//        }
-
-		//"/Users/juconghe/Downloads/commons-lang-LANG_2_5/build.xml"
 		File buildFile = new File("commons-math-build.xml");
 		File outputFile = new File("build.properties");
 		PropertyWriter pw = new PropertyWriter("ant", buildFile, outputFile, "commons-math");
