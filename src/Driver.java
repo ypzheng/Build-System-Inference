edcdaf6b3495ded5db9606f8d62fe00b99a3e2c6
ypzheng <ypzheng@umass.edu>
2017-12-11 22:17:08 -0500

    Driver works for raw build files, Driver_2 works for project directory containing build files.

--- src/Driver.java
+++ src/Driver.java
@@ -1,49 +1,40 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.Scanner;
 
 import util.WildCardResolver;
 
-import java.nio.file.Paths;
-
 public class Driver {
 
-	public static void main(String[] args) throws IOException {
-//		String path = "/Users/caitlynzheng/eclipse-workspace/closure-compiler";
-		String path = "/Users/caitlynzheng/eclipse-workspace/BSI/test/";
-//		String path = "";
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		String path = "";
+		int count = 0;
+		String projectPath = "";
 		File buildFile;
 		
-		System.out.println("Please input your project path: ");
+		System.out.println("Please input a Dir contain projects: ");
 		Scanner scanner = new Scanner(System.in);
-//		path = scanner.nextLine();
+		path = scanner.nextLine();
 		
-		String[] includes = {"*build*.xml","*Build*.xml"};
-		String[] excludes = {};
-		String str[] = WildCardResolver.resolveWildCard(includes, excludes, path);
-		System.out.println(str.length);
-		if(str.length == 1) {
-			buildFile = new File(Paths.get(path).toString() + Paths.get("/") + str[0]);
+		File folder = new File(path);
+		if(!folder.isDirectory()) {
+			System.out.println("Not a dir");
 		}
-		else if(str.length == 0) {
-			System.out.println("No build file found, please manually input your build file name: ");
-			int index = path.lastIndexOf(Paths.get("/").toString());
-			buildFile = new File(Paths.get(path.substring(0,index+1)) + Paths.get("/").toString() + scanner.nextLine());
+		else {
+			for(final File dirs:folder.listFiles()) {
+				if(!dirs.isDirectory()) {
+					buildFile = new File(dirs.toString());
+					File outputFile = new File("build-"+count+".properties");
+					count++;
+					PropertyWriter pw = new PropertyWriter("ant", buildFile, outputFile, Paths.get(path).toString());
+					pw.setProperties();
+				}
+				
+			}
 			
 		}
-		else{
-			System.out.println("More than 1 *build.xml files found, please manually input your build file name:");
-			int index = path.lastIndexOf(Paths.get("/").toString());
-			buildFile = new File(Paths.get(path.substring(0,index+1)) + Paths.get("/").toString() + scanner.nextLine());
-		}
-			
-		File outputFile = new File("build.properties");
-		PropertyWriter pw = new PropertyWriter("ant", buildFile, outputFile, Paths.get(path).toString());
-		pw.setProperties();
 		
 	}
 
